4859ab5fa0e5f58b3a62b135f064ac7a22e557ac
Aleksandr Kolesov <aleksandrkolesov@Aleksandrs-MacBook-Pro.local>
2025-09-17 19:42:06 +0300

    type translation training

--- apps/mobile/app/authorized/learning/type_translation.tsx
+++ apps/mobile/app/authorized/learning/type_translation.tsx
@@ -1,13 +1,106 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import { WText } from "@/mob-ui";
-import { styles } from "../../../general.styles";
+import { useContext, useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { GlowingEllipse } from "@/components/GlowingEllipse";
+import { PlayWordButton } from "@/components/PlayWordButton";
+import { TrainingAppWrapper } from "@/components/TrainingAppWrapper";
+import { BackgroundContext } from "@/context/BackgroundContext";
+import { WCard, WCharInput, WCharInputProps, WText, WZStack } from "@/mob-ui";
+import { Colors } from "@/mob-ui/brand/colors";
 
 export default function TypeTranslation() {
+	const { setColor, setOpacity } = useContext(BackgroundContext);
+	const [status, setStatus] = useState<WCharInputProps["status"]>("default");
+
+	const answer = "World";
+
+	useEffect(() => {
+		setColor(Colors.backgrounds.blue);
+		setOpacity(1);
+
+		return () => {
+			setOpacity(0.3);
+		};
+	}, [setColor, setOpacity]);
+
+	const handleAnswerChange = (text: string) => {
+		if (
+			text.length === answer.length &&
+			text.toLowerCase() === answer.toLowerCase()
+		) {
+			setStatus("success");
+		} else if (text.length === answer.length) {
+			setStatus("error");
+		} else {
+			if (answer.toLowerCase().startsWith(text.toLowerCase())) {
+				setStatus("default");
+			} else {
+				setStatus("error");
+			}
+		}
+	};
+
 	return (
-		<SafeAreaView mode="padding" style={styles.page}>
-			<WText mode="primary" size="2xl">
-				Type translation
-			</WText>
-		</SafeAreaView>
+		<TrainingAppWrapper title="Type the translation">
+			<WCard style={styles.container}>
+				<WZStack style={{ overflow: "hidden" }}>
+					<GlowingEllipse />
+					<View style={[StyleSheet.absoluteFill, styles.translationContainer]}>
+						<View style={styles.wordContainer}>
+							<WText mode="primary" weight="bold" size="3xl">
+								Word
+							</WText>
+							<WText mode="secondary" size="xl">
+								[transliteration]
+							</WText>
+						</View>
+						<View style={styles.wordTranslationContainer}>
+							<PlayWordButton />
+						</View>
+					</View>
+				</WZStack>
+			</WCard>
+
+			<View style={styles.buttonsContainer}>
+				<WCharInput
+					length={answer.length}
+					onChangeText={handleAnswerChange}
+					status={status}
+				/>
+			</View>
+		</TrainingAppWrapper>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		width: "100%",
+		padding: 0,
+		marginVertical: 32,
+		backgroundColor: Colors.backgrounds.primaryBackground,
+		flex: 1,
+	},
+	translationContainer: {
+		padding: 24,
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 16,
+	},
+	wordContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 16,
+	},
+	wordTranslationContainer: {
+		justifyContent: "center",
+		alignItems: "center",
+		gap: 64,
+	},
+	buttonsContainer: {
+		width: "100%",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+		alignContent: "stretch",
+		gap: 16,
+	},
+});
