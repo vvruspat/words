734b116f9fc9abddffb3c766d3b2f669ae7dd948
Aleksandr Kolesov <aleksandrkolesov@Aleksandrs-MacBook-Pro.local>
2025-09-22 20:46:13 +0200

    mobile app extracted to its own repo

--- apps/mobile/mob-ui/atoms/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./WButton";
-export * from "./WCard";
-export * from "./WInput";
-export * from "./WText";
-export * from "./WZStack";
--- apps/mobile/models/Category.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from "@nozbe/watermelondb";
-import { field } from "@nozbe/watermelondb/decorators";
-
-export default class Category extends Model {
-	static table = "categories";
-
-	@field("title")
-	title!: string;
-}
