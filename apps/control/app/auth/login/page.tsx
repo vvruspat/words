422705ef0c405e9573205b3958ae8531b93e7021
Aleksandr Kolesov <vvruspat@gmail.com>
2026-02-18 01:07:57 +0100

    auth

--- apps/control/app/auth/login/page.tsx
+++ apps/control/app/auth/login/page.tsx
@@ -1,10 +1,10 @@
+import { MSpinner } from "@repo/uikit";
 import { Suspense } from "react";
-
 import LoginClient from "./LoginClient";
 
 export default function LoginPage() {
 	return (
-		<Suspense fallback={null}>
+		<Suspense fallback={<MSpinner />}>
 			<LoginClient />
 		</Suspense>
 	);
