b9df63e8afb3e0839b7e25a2552f502352c2d26b
Aleksandr Kolesov <vvruspat@gmail.com>
2026-02-21 00:49:54 +0100

    db dump

--- packages/types/src/api/learning/put.ts
+++ packages/types/src/api/learning/put.ts
@@ -1,4 +1,3 @@
 import { paths } from "../../api";
 
-export type PutLearningResponse =
-	paths["/learning"]["put"]["responses"]["200"]["content"]["application/json"];
+export type PutLearningResponse = paths["/learning"]["put"]["responses"]["200"]["content"]["application/json"];
